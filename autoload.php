9daf4f02cc6931e30f6fa410ef832b3d0b661be5
Mateus Porto Prates <mateuspprates@hotmail.com>
2017-08-03 10:57:03 -0300

    segundo commit

--- autoload.php
+++ autoload.php
@@ -26,7 +26,7 @@ require __DIR__.'/../vendor/autoload.php';
 | by a request. The Artisan "optimize" is used to create this file.
 |
 */
-
+// teste de commit
 $compiledPath = __DIR__.'/cache/compiled.php';
 
 if (file_exists($compiledPath)) {
